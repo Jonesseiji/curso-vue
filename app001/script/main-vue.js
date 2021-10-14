1b2d3a25f8f29c652ab19d36e1b3379073875b1b
jonesseiji <jonesseiji9@gmail.com>
2021-10-14 10:03:40 -0300

    instanciando vue e utilizando interpolação no html para incluir a variável criada no vue

---
+++ app001/script/main-vue.js
@@ -0,0 +1,7 @@
+let root = new Vue({ //instanciando Vue
+    el: "#root", //el = elemento (pode ser string, array, etc) e deve ter o id utilizado no html para associá-lo ao vue)
+    data:{ //variáveis da instância Vue
+        nome: "Jones Seiji",
+        idade: 19
+    }
+});
