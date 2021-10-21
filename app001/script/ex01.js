8e575e4eb1084f17bdebfc42f8808b0475dfe21d
jonesseiji <jonesseiji9@gmail.com>
2021-10-21 14:54:02 -0300

    exemplo prático com diretivas e métodos. falta configurar o botão limpar e estilizar botões

---
+++ app001/script/ex01.js
@@ -0,0 +1,18 @@
+//#region Ex01 VUE
+
+//window.alert("teste")
+let root = new Vue({
+    el: "#root",
+    data: {
+        frutas: [],
+        novaFruta:"",
+        totalFrutas: 0
+    },
+    methods: {
+        inserirFruta: function() {
+            this.frutas.push(this.novaFruta),
+        },
+        limparCampos: function(){
+        }
+    }
+});
