8549ea212a34efe72ecbf2256be2e7c6aa6f7bc1
jonesseiji <jonesseiji9@gmail.com>
2021-10-28 13:49:03 -0300

    finalizando ex 02 (DIRETIVAS V-BIND e V-ON)

--- app/script/ex02.js
+++ app/script/ex02.js
@@ -3,5 +3,23 @@ let root = new Vue({
     data: {
         pais: '',
         imagem: ''
+    },
+    methods: {
+        mostrarPortugal: function(){
+            this.pais = 'Portugal'
+            this.imagem = '/app/public/img/band-portugal.png'
+        },
+        
+        mostrarBrasil: function(){
+            this.pais = 'Brasil'
+            this.imagem = '/app/public/img/band-brasil.png'
+        },
+        
+        mostrarJapao: function(){
+            this.pais = 'Japão'
+            this.imagem = '/app/public/img/band-japao.png'
+        }
     }
 });
+
+root.mostrarBrasil();
