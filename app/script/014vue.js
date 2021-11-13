2a8b4011818e459a933876f180de37e36b755346
Jones Seiji <jonesseiji@outlook.com>
2021-11-13 16:16:24 -0300

    mapeando tecla Enter, mas está bugado

--- app/script/014vue.js
+++ app/script/014vue.js
@@ -6,6 +6,14 @@ let root = new Vue({
     },
     methods:{
         adicionar: function(params) {
+            document.addEventListener("keypress", function(send){
+                if(send.key === "Enter"){
+                    const btn = document.querySelector("#send")
+
+                    btn.click();
+                }
+            })
+
             this.nomes.push(this.novoNome)
             this.novoNome = ''
         }
